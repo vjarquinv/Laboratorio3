528ae0d7ff0c7a4b220241079c4f19545f9d638f
Valeria Jarquin <vjarquin99@gmail.com>
2024-04-30 22:13:26 -0600

    Se agregaron comentarios al codigo

--- ej1.c
+++ ej1.c
@@ -4,12 +4,12 @@
 int main(){
 
     int trg, n = 1, i = 1;
-    printf("Los primeros 100 numeros triangulares son:\n");
+    printf("Los primeros 100 números triangulares son:\n");
     while(i<=100){
-        trg = ((n + 1)*n)/2;
+        trg = ((n + 1)*n)/2; // formula para calcular los números triangulares
         printf("%d ", trg);
-        n++;
-        i++;
+        n++; // se actualiza el n para calcular el siguiente número triangular
+        i++; // se incrementa el contador
     }
     printf("\n");
 return 0;
