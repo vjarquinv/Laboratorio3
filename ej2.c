2266ae9210a9646540bf34d4427fcd11a2e4832d
Valeria Jarquin <vjarquin99@gmail.com>
2024-04-30 23:15:17 -0600

    Se agrego la funcion que los usuarios ingresen el numero

--- ej2.c
+++ ej2.c
@@ -5,15 +5,22 @@ int factorial(int n){
     int i = 1;
     while (n > 1) {
         i = i * n;
-        n = n-1;
+        n = n - 1; // La variable n estaba declarada doble, causando un loop infinito
     }
     return i;
 }
 
 
-int main (int argc , char *argv[ ]) {
-int fac4 = factorial(4);
-int fac5 = factorial(5) ;
-printf(" 4! = %d, 5! = %d\n" , fac4, fac5) ;
+int main (int argc , char *argv[]) {
+    /*
+    int fac4 = factorial(4);
+    int fac5 = factorial(5) ;
+    printf(" 4! = %d, 5! = %d\n" , fac4, fac5);
+    */
+    int fac, facn;
+    printf("Ingrese el número al que desee calcular factorial: "); // Se le solicita al usuario que ingrese un número
+    scanf("%d", &fac);
+    facn = factorial(fac); //Llama la funcion factorial
+    printf("El factorial de %d es %d\n" , fac, facn);
 return 0;
 }
